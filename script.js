ece24faef947e7f1ad91b76fd9c76a557665ef15
eris4444 <147773613+eris4444@users.noreply.github.com>
2025-02-07 22:48:11 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleLanguage() {
+    let title = document.getElementById("title");
+    let description = document.getElementById("description");
+    let button = document.querySelector(".lang-btn");
+
+    if (title.innerText === "من یک برنامه‌نویس پایتون هستم") {
+        title.innerText = "I'm a Python Developer";
+        description.innerText = "My expertise is in cybersecurity and network hacking.";
+        button.innerText = "فارسی";
+    } else {
+        title.innerText = "من یک برنامه‌نویس پایتون هستم";
+        description.innerText = "تخصص من امنیت سایبری و هک شبکه است.";
+        button.innerText = "English";
+    }
+}
