56c10470ebad95a449da2ea39ec981942811b24b
eris4444 <147773613+eris4444@users.noreply.github.com>
2025-02-07 23:18:44 +0330

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ function toggleLanguage() {
         button.innerText = "English";
     }
 }
+
+function toggleMenu() {
+    let menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
